92ae66470f0ccbc83f6c0f500f8be390a3c5b9ec
Pranith chowdary Karumanchi <pranithtpm@gmail.com>
2024-08-18 20:35:15 +0530

    final

--- client/src/components/Browseplan.js
+++ client/src/components/Browseplan.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import plan from '../assets/test.json'//
 import axios from 'axios';
 import { Container, 
   Box,
